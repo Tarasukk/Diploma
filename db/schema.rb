5e3ae1271b28b26545ffebaeb814768859a272c6
Taras <relaxsonik@gmail.com>
2025-04-12 11:38:27 +0300

    add new fields to user upd forms, fix tailwind in dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_10_192537) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_10_205142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_10_192537) do
     t.string "provider"
     t.string "uid"
     t.string "role"
+    t.string "name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
