b8fda7ecf9ea94831ae9dcc966db4814d3ece497
Taras <relaxsonik@gmail.com>
2025-05-07 22:19:08 +0300

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "diploma_production"
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: 587,
